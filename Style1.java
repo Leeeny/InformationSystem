aee72266de492bd054fcffd14bcb94595c624a84
Aleksandr2909 <95142320+Aleksandr2909@users.noreply.github.com>
2021-12-01 19:10:22 +0300

    Add files via upload

---
+++ Style1.java
@@ -0,0 +1,14 @@
+public class Style {
+    private String nameOfStyle;
+}
+
+    public Style(String nameOfStyle) {
+        this.nameOfStyle = nameOfStyle;
+           }
+
+    @Override
+    public String toString() {
+        return "["nameOfStyle+ "]";
+    }
+
+}
---
+++ Track1.java
@@ -0,0 +1,25 @@
+public class Track {
+   private String nameOfTrack;
+    private String artist;
+    private String album;
+    private Long time; //max = 600
+    private Style nameOfStyle;
+    public Style getStyle() {
+        return nameOfStyle;
+    }
+    public void setStyle(Style nameOfStyle ) {
+        this.nameOfStyle = nameOfStyle;
+    }
+    public Track(String nameOfTrack, String artist, String album , Long time) {
+        this.nameOfTrack = nameOfTrack;
+        this.artist = artist;
+        this.album = album;
+        this.time = time;
+
+}
+    @Override
+    public String toString() {
+        return "Track [nameOfTrack=" + nameOfTrack + ", artist=" + artist + ", album=" + album
+                +  ", time=" + time
+                + ", nameOfStyle" + nameOfStyle + "]";
+    }
