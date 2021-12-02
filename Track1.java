770cfb933b39ba9030a1c1ab980ffe97859fbd31
Aleksandr2909 <95142320+Aleksandr2909@users.noreply.github.com>
2021-12-02 14:36:21 +0300

    Update Track1.java

--- Track1.java
+++ Track1.java
@@ -3,23 +3,44 @@ public class Track {
     private String artist;
     private String album;
     private Long time; //max = 600
-    private Style nameOfStyle;
-    public Style getStyle() {
-        return nameOfStyle;
-    }
-    public void setStyle(Style nameOfStyle ) {
-        this.nameOfStyle = nameOfStyle;
-    }
-    public Track(String nameOfTrack, String artist, String album , Long time) {
+
+        public Track(String nameOfTrack, String artist, String album , Long time) {
         this.nameOfTrack = nameOfTrack;
         this.artist = artist;
         this.album = album;
         this.time = time;
 
 }
+    public String getNameOfTrack() {
+        return nameOfTrack;
+    }
+
+    public void setNameOfTrack(String nameOfTrack) {
+        this.nameOfTrack = nameOfTrack;
+    }
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+
     @Override
     public String toString() {
-        return "Track [nameOfTrack=" + nameOfTrack + ", artist=" + artist + ", album=" + album
-                +  ", time=" + time
-                + ", nameOfStyle" + nameOfStyle + "]";
-    }
+        return "Track ['nameOfTrack'][0]['artist'][0]['album'][0]['time'][0]";
