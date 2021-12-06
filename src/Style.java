669852577f4a6049f24b253a8cc5154894876a7c
Нора Нора <issiandra@live.ru>
2021-12-06 15:34:15 +0400

    Починила модель

--- src/Style.java
+++ src/Style.java
@@ -1,3 +1,13 @@
 public class Style {
-    String nameOfStyle;
+    private String nameOfStyle;
+
+    public Style(String nameOfStyle) {
+        this.nameOfStyle = nameOfStyle;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + nameOfStyle + "]";
+    }
+
 }
