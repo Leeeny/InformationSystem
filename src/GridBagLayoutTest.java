5b87b496b030470c54c2d129d6a746f93716a02a
Aleksandr2909 <95142320+Aleksandr2909@users.noreply.github.com>
2022-02-18 21:01:58 +0300

    Update GridBagLayoutTest.java

--- src/GridBagLayoutTest.java
+++ src/GridBagLayoutTest.java
@@ -1,14 +1,17 @@
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.JScrollPane;
 
-public class GridBagLayoutTest {
+public class GridBagLayoutTest
+{
+
+
 
     public static void createPanelUI(Container container) {
         JButton button;
         JButton button1;
+
         JTable table;
         JScrollPane scrollPane;
         JRadioButton radio1;
@@ -16,7 +19,6 @@ public class GridBagLayoutTest {
         JRadioButton radio3;
         JTextField input;
         JLabel label;
-        //ButtonGroup group;
 
 
         container.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
@@ -31,31 +33,34 @@ public class GridBagLayoutTest {
 
         input = new JTextField("", 5);
         constraints.gridwidth = 2;
-        constraints.gridy = 5;      // нулевая ячейка таблицы по горизонтали
+        constraints.gridy = 5;      
         container.add(input, constraints);
 
-
         label = new JLabel("Input:");
-        constraints.gridy = 4;      // нулевая ячейка таблицы по горизонтали
+        constraints.gridy = 4;      
         container.add(label, constraints);
 
 
         radio1 = new JRadioButton("Change track");
         constraints.gridx = 0;
-        constraints.gridy = 1;      // нулевая ячейка таблицы по горизонтали
+        constraints.gridy = 1;      
         container.add(radio1, constraints);
+        //radio1.addActionListener(new ButtonEventListener());
+
 
         radio2 = new JRadioButton("Delete track");
         constraints.gridx = 0;
-        constraints.gridy = 2;      // первая ячейка таблицы по горизонтали
+        constraints.gridy = 2;      
         container.add(radio2, constraints);
 
         radio3 = new JRadioButton("Add track");
         constraints.gridx = 0;
-        constraints.gridy = 3;      // вторая ячейка таблицы по горизонтали
+        constraints.gridy = 3;     
         container.add(radio3, constraints);
 
-        //button = new JButton("Высокая кнопка размером в 2 ячейки");
+
+
+
 
 
         String[] columnNames = {
@@ -80,9 +85,11 @@ public class GridBagLayoutTest {
         };
 
 
+
+
         table = new JTable(data, columnNames);
         constraints.ipady = 2;   // кнопка высокая
-        constraints.weightx = 0.5;
+        constraints.weightx   = 0.5;
         constraints.gridwidth = 2;    // размер кнопки в две ячейки
         constraints.gridx = 0;    // нулевая ячейка по горизонтали
         constraints.gridy = 0;    // первая ячейка по вертикали
@@ -95,7 +102,7 @@ public class GridBagLayoutTest {
         //container.add(new JScrollPane(table));
 
 
-        button = new JButton("Press");
+        button1 = new JButton("Press");
         //constraints.ipady     = 0;    // установить первоначальный размер кнопки
         //constraints.weighty   = 1.0;  // установить отступ
         // установить кнопку в конец окна
@@ -103,51 +110,88 @@ public class GridBagLayoutTest {
         //constraints.insets    = new Insets(5, 5, 5, 5);  // граница ячейки по Y
         constraints.gridwidth = 1;    // размер кнопки в 2 ячейки
         constraints.gridx = 0;    // первая ячейка таблицы по горизонтали
-        constraints.gridy = 6;    // вторая ячейка по вертикали
-        container.add(button, constraints);
+        constraints.gridy = 6;    //  ячейка по вертикали
+        container.add(button1, constraints);
 
-        button1 = new JButton("Save playlist");
-        //constraints.ipady     = 0;    // установить первоначальный размер кнопки
-        //constraints.weighty   = 1.0;  // установить отступ
+        button= new JButton("Save playlist");
+        //constraints.ipady     = 0;    
+        //constraints.weighty   = 1.0;  
         // установить кнопку в конец окна
         //constraints.anchor    = GridBagConstraints.PAGE_END;
         //constraints.insets    = new Insets(5, 0, 0, 0);  // граница ячейки по Y
         //constraints.gridwidth = 2;    // размер кнопки в 2 ячейки
-        constraints.gridx = 1;    // первая ячейка таблицы по горизонтали
-        constraints.gridy = 6;    // вторая ячейка по вертикали
-        container.add(button1, constraints);
+        constraints.gridx = 1;    
+        constraints.gridy = 6;    
+        container.add(button, constraints);
 
-        ButtonGroup group = new ButtonGroup();
-        group.add(radio1);
-        group.add(radio2);
-        group.add(radio3);
-        container.add(radio1);
-        radio1.setSelected(true);
-        container.add(radio2);
-        radio2.setSelected(true);
-        container.add(radio3);
-        radio3.setSelected(true);
-        //button.addActionListener(new ButtonEventListener());
-        button.addActionListener(new ActionListener() {
+        button1.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 String message = "";
                 message += "Button was pressed\n";
+
+                JOptionPane.showMessageDialog(null, message, "Output", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = "";
                 message += "Playlist saved\n";
-                message += "Text is" + input.getText() + "\n";
-                message += (radio1.isSelected() ? "Radio #1" : "Radio #2") + "is selected!\n";
-                message += (radio2.isSelected() ? "Radio #2" : "Radio #3") + "is selected!\n";
-                //message += "Checkbox is" + ((check.isSelected()) ? "checked" : "unchecked");
+
+                JOptionPane.showMessageDialog(null, message, "Output", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+        radio1.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = "";
+                message += "Track changed\n";
+
+                JOptionPane.showMessageDialog(null, message, "Output", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+        radio2.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = "";
+                message += "Track deleted\n";
+
+                JOptionPane.showMessageDialog(null, message, "Output", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+        radio3.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = "";
+                message += "Track added\n";
+
+                JOptionPane.showMessageDialog(null, message, "Output", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+        input.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = "";
+                message += "track is "+input.getText()+"\n";
+
                 JOptionPane.showMessageDialog(null, message, "Output", JOptionPane.PLAIN_MESSAGE);
             }
         });
 
-        container.add(button);
-        container.add(button1);
+
     }
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args)
+    {
         // Создание окна
         JFrame frame = new JFrame("GridBagLayoutTest");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -160,3 +204,5 @@ public class GridBagLayoutTest {
         frame.setVisible(true);
     }
 }
+
+
